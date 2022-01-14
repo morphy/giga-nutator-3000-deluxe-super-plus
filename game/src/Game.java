d772d30e7e6de111759a225271013e09a380f48f
morphy <you@example.com>
2022-01-14 07:05:32 +0100

    Started working on GUI

---
+++ game/src/Game.java
@@ -0,0 +1,11 @@
+import javax.sound.midi.MidiChannel;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Synthesizer;
+import java.awt.*;
+
+public class Game {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(Frame::new);
+    }
+}
